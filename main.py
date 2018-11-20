eb193912824af062915022b14cfbfcb1624134ec
Paszymaja <sprzepiora@hotmail.co.uk>
2018-11-20 11:23:39 +0100

    MacierzB, transponowanie działa cała reszta raczej nie.

--- main.py
+++ main.py
@@ -25,15 +25,41 @@ def linearyzjaca(macierz, v):
     for i in range(0, 8):
         for j in range(0, 4):
             if j != 3:
-                macierz[i][j] = 2*(-macierz[i][j]+macierz[0][j])
+                macierz[i][j] = 2*(-(macierz[i][j])+macierz[0][j])
             else:
-                macierz[i][j] = pow(v, 2)*2*(macierz[i][j]-macierz[0][j])
+                macierz[i][j] = pow(v, 2)*2*(macierz[i][j]-(macierz[0][j]))
+    macierz = np.delete(macierz, 0, 0)
     return macierz
 
 
+def macierz_b(macierz, v):
+    tab = []
+    for i in range(0, 8):
+        for j in range(0, 4):
+            if j != 3:
+                a = -(pow(macierz[i][j], 2)) + pow(macierz[0][j], 2)
+            else:
+                b = pow(v, 2)*(pow(macierz[i][j], 2) - pow(macierz[0][j], 2))
+        tab.append(a+b)
+    mat = np.array(tab).reshape(8, 1)
+    mat = np.delete(mat, 0, 0)
+    return mat
+
+
+macierzB = macierz_b(macierzA, wave_speed)
 macierzA = linearyzjaca(macierzA, wave_speed)
-macierzA = np.delete(macierzA, 0, 0)
-print(macierzA)
+print(macierzA, "\n")
+print(macierzB, "\n")
+
+macierzT = macierzA.transpose()
+print(macierzT, "\n")
+macierzA = np.dot(macierzT, macierzA)
+macierzB = np.dot(macierzT, macierzB)
+print(macierzA, "\n")
+print(macierzB, "\n")
+
+print(np.linalg.solve(macierzA, macierzB))
+
 
 
 
